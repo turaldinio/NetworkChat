9165e4e0ae0248ba02073f02265fce8f7dd30fe1
Tural Guluev <tural-98@bk.ru>
2022-08-30 17:28:43 +0300

    add Connection class (wrapper over socekt

---
+++ src/main/java/Connection.java
@@ -0,0 +1,50 @@
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Connection implements Closeable {
+    private final Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
+    public Connection(Socket socket) {
+        this.socket = socket;
+        try {
+            this.out = new ObjectOutputStream(socket.getOutputStream());
+            this.in = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void send(Message message) {
+        try {
+            out.writeObject(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized Message receive() {
+        try {
+            return (Message) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public SocketAddress getRemoteSocketAddress() {
+        return socket.getRemoteSocketAddress();
+    }
+
+
+    @Override
+    public void close() throws IOException {
+        out.close();
+        in.close();
+    }
+}
