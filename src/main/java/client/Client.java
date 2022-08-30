5d417ace131391bcad1c1f79e40331a5faf878a2
Tural Guluev <tural-98@bk.ru>
2022-08-30 19:49:08 +0300

    added auxiliary methods in Client class

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,12 +1,30 @@
 package client;
 
 import sharedResources.Connection;
+import sharedResources.Message;
+import sharedResources.MessageType;
+
+import java.util.Scanner;
 
 public class Client {
     protected Connection connection;
-    private boolean clientConnected=false;
+    private boolean clientConnected = false;
 
     public class SocketThread extends Thread {
 
     }
+
+    protected String getClientName() {
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        connection.send(new Message(text, MessageType.TEXT));
+    }
+
 }
