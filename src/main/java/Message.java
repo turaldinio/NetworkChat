bde11feaf3408b82e40e62034f66e076dbf405be
Tural Guluev <tural-98@bk.ru>
2022-08-30 17:23:54 +0300

    add Message class

---
+++ src/main/java/Message.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private String text;
+    private MessageType messageType;
+
+    public Message(String text, MessageType messageType) {
+        this.text = text;
+        this.messageType = messageType;
+    }
+
+    public Message(MessageType messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+}
